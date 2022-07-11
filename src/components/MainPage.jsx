d07aa09a2dc05fbf5a46b36551bd731db339807d
ravi-2403 <ravidtg3@gmail.com>
2022-07-11 19:16:37 +0530

    included the api involvment

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import {React,useState} from 'react'
 import './MainPage.css'
 import {useNavigate,
         Outlet,
-        } from 'react-router-dom'
+  } from 'react-router-dom'
+import axios from 'axios'
+
 
 function MainPage() {
-
+  const [entryData,setEntryData] = useState({});
   let navigate = useNavigate();
-  function handleClick(){
-    navigate("/show-login-popup")
+
+
+  const handleClick = async ()=>{
+
+    const response = await axios.get('https://randomuser.me/api');
+    setEntryData(response.data.results[0])
+    var entry = {name:`${entryData.name.title} ${entryData.name.first} ${entryData.name.last}`,
+                 email:`${entryData.email}`,
+                 address:`${entryData.location.city},${entryData.location.country}`,
+                 phone:`${entryData.phone}`,
+                 profilePicture:`${entryData.picture.large}`,
+                 gender:`${entryData.gender}`
+               }
+    console.log(entryData);
+    navigate("/show-login-popup",{state:entry})
   }
-
-
 
   return (
     <div style={{position:"relative"}}>
--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import {React} from 'react'
 import './PopUp.css'
-import {useNavigate} from 'react-router-dom'
+import {useNavigate,
+        useLocation,
+} from 'react-router-dom'
+
+
 function PopUp(props) {
   let navigate = useNavigate();
+  let location = useLocation();
+
+
   function handleclose(props){
+    console.log(location.state);
     navigate('/');
-  } 
+  }
+
   return (
     <div className='main'>
       <div className='main-inner-div'>
         <div className='main-inner-div-item'>
           <div className='main-inner-div-item-text'>
-            <h1>Name : Atul Kumar</h1>
-            <h3>Email : ravikdtg@gmail.com</h3>
-            <h3>Address : Bangalore,India</h3>
-            <h3>Phone : 7484906645</h3>
-            <h3>Intrest : Swimming</h3>
+            <h1>Name : {location.state.name}</h1>
+            <h3>Email : {location.state.email}</h3>
+            <h3>Address : {location.state.address}</h3>
+            <h3>Phone : {location.state.phone}</h3>
+            <h3>Gender : {location.state.gender}</h3>
           </div>
+          <img src={location.state.profilePicture}
+               alt="profilePicture  not found"
+               className="rounded-circle img-thumbnail border-danger"
+               />
           <button onClick={handleclose}>close</button>
         </div>
       </div>
